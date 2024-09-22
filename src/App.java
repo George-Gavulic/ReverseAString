38612b9b1a8084fcadcfd048f4f7d976b695f11e
George-Gavulic <georgegav522@gmail.com>
2024-09-22 11:47:57 -0400

    third commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String forward;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input a word you would like to reverse:");
+        forward = scanner.nextLine();
+        
+        scanner.close();
+        
+        System.out.println(forward);
     }
 }
