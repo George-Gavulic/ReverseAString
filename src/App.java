66c52f3171a562c84f4bf614ec23de892645eb59
George-Gavulic <georgegav522@gmail.com>
2024-09-22 11:58:00 -0400

    final commit

--- src/App.java
+++ src/App.java
@@ -5,11 +5,12 @@ public class App {
         String forward;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please input a word you would like to reverse:");
+        System.out.println( "\n \nPlease input a word you would like to reverse:");
         forward = scanner.nextLine();
-        
         scanner.close();
         
-        System.out.println(forward);
+        for (int i = forward.length() - 1; i >= 0; i--) {
+            System.out.print(forward.charAt(i));
+        }
     }
 }
